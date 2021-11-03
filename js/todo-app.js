b0ba6945fd14797bcd538a0028a1240442e8b116
MariaLight <mdlight@mail.ru>
2021-11-03 21:02:02 +0300

    localStorage added

--- js/todo-app.js
+++ js/todo-app.js
@@ -1,11 +1,13 @@
 (function () {
     function createAppTitle(title) {
+        //функция формирует заголовок по заданному значению
         let appTitle = document.createElement('h2');
         appTitle.innerHTML = title;
         return appTitle;
     }
 
     function createTodoItemForm() {
+        //функция формирует форму ввода данных
         let form = document.createElement('form');
         let input = document.createElement('input');
         let buttonWrapper = document.createElement('div');
@@ -17,6 +19,8 @@
         buttonWrapper.classList.add('input-group-append');
         button.classList.add('btn', 'btn-primary');
         button.textContent = 'Добавить дело';
+        button.setAttribute('disabled', true); //кнопка недоступна, пока поле пустое
+        
 
         buttonWrapper.append(button);
         form.append(input);
@@ -30,12 +34,14 @@
     }
 
     function createTodoList() {
+        //функция формирует список дел (пока пустой)
         let list = document.createElement('ul');
         list.classList.add('list-group');
         return list;
     }
 
-    function createTodoItem(name) {
+    function createTodoItem(name, status = false) {
+        //функция формирует элемент списка - дело - с двумя кнопками - готово и удалить
         let item = document.createElement('li');
 
         let buttonGroup = document.createElement('div');
@@ -44,6 +50,7 @@
 
 
         item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+        if (status) item.classList.add('list-group-item-success'); //если дело выполнено, так и помечаем
         item.textContent = name;
 
         buttonGroup.classList.add('btn-group', 'btn-group-sm');
@@ -58,25 +65,119 @@
 
         return {
             item,
+            status,
             doneButton,
             deleteButton,
         }
     }
 
-    function createTodoApp(container, title = 'Список дел') {
+
+
+    function checkDone(currentItem, currentList, currentValue, page){
+        //функция отмечает дело как выполненное (или снимает эту метку) и добавляет в локальное хранилище данные об этом
+        currentItem.doneButton.addEventListener('click', function () {
+                currentItem.item.classList.toggle('list-group-item-success');
+                
+                //ищем в списке именно это дело, которое мы выполнили, по имени
+                let i = -1;
+                for (let j = 0; j <currentList.length; j++){
+                    if (currentList[j].name === currentValue){
+                        i = j;
+                        break;
+                    };
+                }       
+
+                //заносим в спиок значение о состоянии дела
+                if (currentItem.item.classList.contains('list-group-item-success')) {
+                    currentList[i].done = true;    
+                }
+                else {
+                    currentList[i].done = false;    
+                }
+                addToLocalItem(currentList, page); //отправляем в хранилище
+
+
+            });
+    }
+
+    function deleteItem(currentItem, currentList, currentValue, page) {
+        //функция удаляет объект с экрана и из хранилища
+        currentItem.deleteButton.addEventListener('click', function () {
+            if (confirm('Вы уверены?')) { 
+                let i = -1;
+                //ищем нужное дело по имени
+                for (let j = 0; j < currentList.length; j++){
+                    if (currentList[j].name === currentValue){
+                        i = j;
+                        break;
+                    };
+                }       
+                //удаляем дело из списка и из хранилища
+                currentItem.item.remove(); 
+                currentList.splice(i, 1); 
+                addToLocalItem(currentList, page);
+
+            }
+        });
+    }
+
+    function addToLocalItem(list, page){
+        //функция обновляет данные в хранилище
+        let listString = JSON.stringify(list);
+        localStorage.removeItem(page);
+        localStorage.setItem(page, listString);
+    }
+
+    function createTodoApp(container, title = 'Список дел', page = 'data') {
+
 
         let todoAppTitle = createAppTitle(title);
         let todoItemForm = createTodoItemForm();
         let todoList = createTodoList();
+        let todoListSave = [];
+
 
         container.append(todoAppTitle);
         container.append(todoItemForm.form);
         container.append(todoList);
 
+        //вынимаем элементы из хранилища
+        let localData;
+        localData = localStorage.getItem(page);
+        localData = JSON.parse(localData);
+
+        //если элементы есть, то каждое дело отображаем на экране, 
+        //добавляем в список и даём возможность удалять его и отмечать как выполненное
+        if (localData){
+            let localItem;
+            for (object of localData) {
+                localItem = createTodoItem(object.name, object.done);
+                todoList.append(localItem.item);
+                todoListSave.push(object);
+                checkDone(localItem, todoListSave, object.name, page);
+                deleteItem(localItem, todoListSave, object.name, page);
+            } 
+        }
+        
+
+        //следим за состоянием строки ввода. если что-то появляется - кнопка становится доступной
+        setTimeout(function(){
+            todoItemForm.input.addEventListener('input', function(){
+            if (todoItemForm.input.value) {
+                todoItemForm.button.removeAttribute('disabled');   
+            }
+            else {
+                todoItemForm.button.setAttribute('disabled', true);
+            }
+        })
+        }, 20);
+
+        
 
         todoItemForm.form.addEventListener('submit', function (e) {
+                
+            e.preventDefault(); //предотвращается дефолтное поведение формы (отправка на сервер)
 
-            e.preventDefault();
 
             if (!todoItemForm.input.value) {
                 return;
@@ -85,23 +186,27 @@
             let todoItem = createTodoItem(todoItemForm.input.value);
 
 
-            todoItem.doneButton.addEventListener('click', function () {
-                todoItem.item.classList.toggle('list-group-item-success');
-            });
+            //формируем список по значениям
+            let todoItemValue = todoItemForm.input.value;
+            todoListSave.push({name: todoItemValue, done: false});
 
-            todoItem.deleteButton.addEventListener('click', function () {
-                if (confirm('Вы уверены?')) {
-                    todoItem.item.remove();
-                }
-            });
+            //оживляем кнопки элементов            
+            checkDone(todoItem, todoListSave, todoItemValue, page);
+            deleteItem(todoItem, todoListSave, todoItemValue, page);
 
-            todoList.append(todoItem.item);
+            
+            todoList.append(todoItem.item); //отображаем значение на экране
+            todoItemForm.input.value = ''; //очищаем поле ввода
+            todoItemForm.button.setAttribute('disabled', true); //блокируем кнопку
 
-            todoItemForm.input.value = '';
-        })
+
+
+            addToLocalItem(todoListSave, page); //обновляем данные в хранилище - добавляем новый элемент
+        })   
+        
 
     }
 
-    window.createTodoApp = createTodoApp;
+    window.createTodoApp = createTodoApp; 
 
 })();
